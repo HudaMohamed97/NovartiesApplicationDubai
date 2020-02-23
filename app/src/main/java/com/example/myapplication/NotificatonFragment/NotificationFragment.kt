4b5a6acd101743e22c846dd3f7541d84c0de3408
Huda Mohamed <huda.fci@gmail.com>
2020-02-23 08:50:21 -0800

    Add Notification

--- app/src/main/java/com/example/myapplication/NotificatonFragment/NotificationFragment.kt
+++ app/src/main/java/com/example/myapplication/NotificatonFragment/NotificationFragment.kt
@@ -1,7 +1,5 @@
 package com.example.myapplication.NotificatonFragment
 
-
-import android.app.Notification
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
